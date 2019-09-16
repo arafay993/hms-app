9a33581461bf3e9fb18a61354a1b3db972d38df3
Abdul Rafay Khan <abdulrafay993@gmail.com>
2019-09-16 20:03:22 +0500

    Added Ingredients Model

---
+++ server/models/ingredients.js
@@ -0,0 +1,38 @@
+export default (sequelize, Sequelize) => {
+	const Ingredient = sequelize.define("ingredient", {
+		label: {
+			type: Sequelize.STRING(500),
+            allowNull: false,
+            notEmpty: false,
+			comment: "Name of Ingredient"
+		},
+        group: {
+			type: Sequelize.STRING(500),
+            allowNull: false,
+            notEmpty: false,
+			comment: "Group to which this Ingredient belongs"
+		},
+		price: {
+			type: Sequelize.FLOAT(30),
+            allowNull: true,
+            notEmpty: true,
+			comment: "Price of the Ingredient"
+		}
+	}, {
+		tableName: "ingredient",
+		freezeTableName: true
+    });
+    
+    Ingredient.associate = (models) => {
+        Ingredient.hasMany(models.ingredient, {
+            onDelete: 'CASCADE',
+            foreignKey: {
+                name: 'parent_id',
+                allowNull: true,
+                notEmpty: false,
+            },
+            as: 'madeof'
+        });
+    }
+    return Ingredient;
+}
